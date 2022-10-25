6a35a4e9dcfed93383a8be5b3a7ff397d1542a18
Caleb Apronti <chindrop@gmail.com>
2022-10-26 01:36:06 +0200

    Created App

--- app.py
+++ app.py
@@ -9,12 +9,32 @@ from torch import autocast
 from diffusers import StableDiffusionPipeline
 
 app = tk.Tk()
-app.geometry("532x662")
+app.geometry("532x632")
 app.title("Text2Image")
 ctk.set_appearance_mode("dark")
 
 prompt = ctk.CTkEntry(height=40, width=512, text_font=("Arial", 20), text_color="black", fg_color="white")
 prompt.place(x=10, y=10)
+
+lmain = ctk.CTkLabel(height=512, width=512)
+lmain.place(x=10, y=110)
+
+modelid = "CompVis/stable-diffusion-v1-4"
+device="cuda"
+pipe = StableDiffusionPipeline.from_pretrained(modelid, revision="fp16", torch_dtype=torch.float16, use_auth_token=auth_token)
+pipe.to(device)
+
+def generate():
+    with autocast(device):
+        image = pipe(prompt.get(), guidance_scale=8.5)["sample"][0]
+
+    image.save('generatedimage.png')
+    img = ImageTk.PhotoImage(image)
+    lmain.configure(image=img)
+
+button = ctk.CTkButton(height=40, width=120, text_font=("Arial", 20), text_color="white", fg_color="green", command=generate)
+button.configure(text="Generate")
+button.place(x=206, y=60)
 
 
 app.mainloop()
